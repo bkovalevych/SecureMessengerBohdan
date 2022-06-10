9e3c92a18c6370519c8e38d4c66e41829559ac26
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-06-10 13:28:31 +0300

    added settings for mongo

---
+++ SecureMessengerBohdan/Extensions/WebApplicationBuilderSetSettingsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using SecureMessengerBohdan.Settings;
+
+namespace SecureMessengerBohdan.Extensions
+{
+    public static class WebApplicationBuilderSetSettingsExtensions
+    {
+        public static WebApplicationBuilder SetSettings(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<MongoDbConfig>(conf =>
+            builder.Configuration.GetSection(nameof(MongoDbConfig))
+            .Bind(conf));
+
+            return builder;
+        }
+    }
+}
