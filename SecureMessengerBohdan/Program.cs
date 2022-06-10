c30e6ac6112676fd060fc5b9c691344b977913c5
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-06-10 13:34:07 +0300

    added settings

--- SecureMessengerBohdan/Program.cs
+++ SecureMessengerBohdan/Program.cs
@@ -1,4 +1,8 @@
+using SecureMessengerBohdan.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.SetSettings();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
